84b809e1b4226fb62b62701592d580eb9d7599d9
sujinpark <asgol1120@naver.com>
2024-03-05 21:38:30 +0900

    ch4

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,11 +1,21 @@
 import { API_URL } from "../app/(home)/page";
-
-async function getMovie(id:string) {
+import styles from "../styles/movie-info.module.css"
+export async function getMovie(id:string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json()
 }
 
 export default async function MovieInfo({id}: {id:string}){
   const movies = await getMovie(id)
-  return <h6>{JSON.stringify(movies)}</h6>
+  return (
+    <div className={styles.container}>
+      <img className={styles.poster} src={movies.poster_path} alt={movies.title}/>
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movies.title}</h1>
+        <h3>⭐️{movies.vote_average.toFixed(1)}</h3>
+        <p>{movies.overview}</p>
+        <a href={movies.homepage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  )
 }
